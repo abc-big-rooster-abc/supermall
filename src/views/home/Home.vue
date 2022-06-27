3adccac447df2bbdff2d726a3321424c93b1c0f5
xiegongsheng <1581223071@qq.com>
2022-06-27 23:16:25 +0800

    编写请求axios,数据提取

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,17 +8,39 @@
 
 <script>
     import NavBar from "components/common/navbar/NavBar";
+    import {getHomeMultiData} from "network/home"
     export default {
         name: "home",
         components:{
           NavBar,
+        },
+        data(){
+          return{
+            banners: [],
+            products: [],}
+        },
+        // 请求多个数据
+        created(){
+         this.getHomeData()
+        },
+        methods:{
+            getHomeData() {
+                getHomeMultiData().then(res => {
+                    console.log(res.data.banner);
+                    console.log(res.data.recommend);
+                    this.banners = res.data.banners
+                    this.products = res.data.recommend
+                })
+            },
         }
+
     }
 </script>
 
 <style scoped>
   #home{
     background-color: var(--color-tint);
-    color:white
+    color:white;
+
   }
 </style>
